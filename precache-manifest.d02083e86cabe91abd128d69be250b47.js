840177a85f212e7825affe21e4375f2fcf0a950b
Alex Chan <alexander.m.chan@gmail.com>
2019-05-17 12:21:36 -0600

    Updates

---
+++ precache-manifest.d02083e86cabe91abd128d69be250b47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e90df24f2cd2cefee8753368cfdb6b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5d3f83a26463e8b9748",
+    "url": "/static/css/main.558bbccf.chunk.css"
+  },
+  {
+    "revision": "9b1d285d4a80bfa85076",
+    "url": "/static/js/2.5a2a5192.chunk.js"
+  },
+  {
+    "revision": "c5d3f83a26463e8b9748",
+    "url": "/static/js/main.ae64caa2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
